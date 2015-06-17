fd29ef8219a35520e862ec6d6e36815d64107304
xuxl <xuxle@yonyou.com>
2015-06-17 14:44:48 +0800

    inAppBrowser

--- www/apps/ngCordova/inAppBrowser/inAppBrowser.js
+++ www/apps/ngCordova/inAppBrowser/inAppBrowser.js
@@ -2,18 +2,26 @@
 define(['ionic'], function () {
     angular.module('demo.inAppBrowser', [])
 
-        .controller('inAppBrowserCtrl', ['$scope', '$cordovaInAppBrowser', function ($scope, $cordovaInAppBrowser) {
+        .controller('inAppBrowserCtrl', ['$rootScope', '$scope', '$cordovaInAppBrowser', function ($rootScope, $scope, $cordovaInAppBrowser) {
             $scope.url = 'http://www.baidu.com';
             $scope.target = '_blank';
+
             $scope.openUrl = function () {
                 var options = {
                     location: "no"
                 };
-                $cordovaInAppBrowser.open($scope.url, '_blank', options).then(function () {
+                $cordovaInAppBrowser.open($scope.url, $scope.target, options).then(function () {
                     //$cordovaToast.showShortBottom("InAppBrowser opened http://ngcordova.com successfully");
                 }, function (error) {
                     //$cordovaToast.showShortBottom("Error: " + error);
                 });
             };
+
+            $rootScope.$on("$cordovaInAppBrowser:exit", function (event, result) {
+                alert(result.type + ', ' + result.additional);
+            });
+            //$rootScope.$on("$cordovaInAppBrowser:loaderror", function (event, result) {
+            //    alert(result.code + ', ' + result.message);
+            //});
         }]);
 });
