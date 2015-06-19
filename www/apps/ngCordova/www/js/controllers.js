661ee31a9717529afb1379566266ea2c3da8306e
xuxl <xuxle@yonyou.com>
2015-06-19 10:54:49 +0800

    inAppBrowser：return main app stable

--- www/apps/ngCordova/www/js/controllers.js
+++ www/apps/ngCordova/www/js/controllers.js
@@ -1,4 +1,7 @@
 angular.module('starter.controllers', [])
 
     .controller('HomeCtrl', function ($scope) {
+        $scope.goMainApp = function (url) {
+            window.open(url, '_blank', 'location=no');
+        };
     });
